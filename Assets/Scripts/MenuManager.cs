ed2da270b9fdddf1496395f9307676d0bc627df1
Valerij Dobler <Valerij.Dobler@studium.uni-hamburg.de>
2019-08-07 16:48:36 +0200

    Add Motivauswahl

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject AuswahlPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AuswahlPanel.SetActive(false);
+    }
+
+    public void HideAuswahl()
+    {
+        AuswahlPanel.SetActive(false);
+    }
+
+    public void ShowAuswahlPanel()
+    {
+        AuswahlPanel.SetActive(true);
+    }
+
+    IEnumerator AnimateAuswahlPanel()
+    {
+        return null;
+    }
+
+    public void QuitApplication()
+    {
+        Debug.Log("Exiting...");
+#if UNITY_EDITOR
+         UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
